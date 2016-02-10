9ad4c3f074a31fba9a49020b55fc821a5e020cff
Jake Dexheimer <jakedex@gmail.com>
2016-02-10 01:44:28 -0600

    Setup sections, initial pinning

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+(function($) {
+ 
+  // Init ScrollMagic
+  var ctrl = new ScrollMagic.Controller({
+      globalSceneOptions: {
+          triggerHook: 'onLeave'
+      }
+  });
+  
+   // Create scene
+  $("section").each(function() {
+
+      var name = $(this).attr('id');
+
+      new ScrollMagic.Scene({
+          triggerElement: this
+      })
+      .setPin(this)
+      .addIndicators()
+      .addTo(ctrl);
+
+  });
+  
+  
+ 
+})(jQuery);
